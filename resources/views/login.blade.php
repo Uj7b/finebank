53ff666351486900f62a0368dd89ed6b7875cde1
Uj7b <107117746+Uj7b@users.noreply.github.com>
2025-11-14 16:44:36 +0100

    html/css structure set

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,28 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    @vite('resources/css/login.css')
     <title>Document</title>
 </head>
 <body>
-    <h1><span>FINE</span>bank.<span>IO</span></h1>
-    <div>
+    <div class="container">
+        <h1><span class="bold">FINE</span>bank.<span class="bold">IO</span></h1>
         <form>
-            <label for="">Email Address</label>
-            <input type="email" name="" id="">
+            <label for="email">Email Address</label>
+            <input type="email" name="email" id="email" placeholder="johndoe@gmail.com">
             <div>
-                <label for="">Password</label>
-                <a href="">Forgot Password</a>
+                <label for="password">Password</label>
+                <a href="" class="forget-password">Forgot Password?</a>
             </div>
-            <input type="password" name="" id="">
+            <input type="password" name="password" id="password" placeholder="●●●●●●●●●●●●●●">
             <input type="checkbox" name="" id="">
             <label for="">Keep me signed in</label>
-            <input type="submit">
+            <input type="submit" value="Login">
         </form>
-        <div>
-            <hr>or sign in with<hr>
+        <div class="divider">
+            <hr><span>or sign in with</span><hr>
         </div>
         <button>Continue with Google</button>
-        <a href="">Create an account</a>
+        <a href="" class="create-account">Create an account</a>
     </div>
 </body>
 </html>
