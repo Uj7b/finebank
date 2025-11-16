42b0b21d07ac25ab9c75fd5fcb3de2ae16609dc8
Uj7b <107117746+Uj7b@users.noreply.github.com>
2025-11-16 11:03:55 +0100

    adding forgot password and welcome page

---
+++ resources/views/forgot-password.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h1><span class="bold">FINE</span>bank.<span class="bold">IO</span></h1>
+        <span>Forgot Password?</span>
+        <p>Enter your email address to get the password reset link</p>
+        <form action="">
+            <label for="">Email Address</label>
+            <input type="email" name="" id="">
+            <input type="submit" value="Password Reset">
+        </form>
+        <a href="">Back to login</a>
+    <div>
+</body>
+</html>
