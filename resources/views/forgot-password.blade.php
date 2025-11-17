fcd2f6d49ae1d1cb02dd4bd9443e375644008871
Uj7b <107117746+Uj7b@users.noreply.github.com>
2025-11-17 15:33:00 +0100

    forgot-password css made up

--- resources/views/forgot-password.blade.php
+++ resources/views/forgot-password.blade.php
@@ -11,11 +11,11 @@
 <body>
     <div class="container">
         <h1><span class="bold">FINE</span>bank.<span class="bold">IO</span></h1>
-        <span>Forgot Password?</span>
-        <p>Enter your email address to get the password reset link</p>
+        <span class="forgot-password">Forgot Password?</span>
+        <p>Enter your email address to get the password reset link.</p>
         <form action="">
             <label for="">Email Address</label>
-            <input type="email" name="" id="">
+            <input type="email" name="" id="" placeholder="hello@example.com">
             <input type="submit" value="Password Reset">
         </form>
         <a href="">Back to login</a>
